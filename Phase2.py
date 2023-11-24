9220dc15983498e1b244887156ce60d6a4547a09
AamirRehman1 <148191554+AamirRehman1@users.noreply.github.com>
2023-11-24 00:00:49 -0800

    Update Phase2.py

--- Phase2.py
+++ Phase2.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    # Use 0.0.0.0 as the host to allow external connections
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
 
