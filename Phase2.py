28a543b1c54c86f576c4728800169794044cac2f
AamirRehman1 <148191554+AamirRehman1@users.noreply.github.com>
2023-11-28 11:32:51 -0800

    Update Phase2.py

--- Phase2.py
+++ Phase2.py
@@ -1,17 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1224567'  # Remember to use a different key for production
+
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Additional routes and socket events
+@app.route('/create_project', methods=['POST'])
+def create_project():
+    project_name = request.form['project_name']
+    return redirect(url_for('wbs_diagram', project_name=project_name))
+
+@app.route('/wbs_diagram')
+def wbs_diagram():
+    project_title = request.args.get('project_name', 'Default Project Name')
+    return render_template('wbs_diagram.html', project_title=project_title)
+
+@socketio.on('connect')
+def test_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
     socketio.run(app)
+
 
 
 
