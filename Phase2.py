6775e2f5a01bb5a53ce4fda40f209fe5dc642527
AamirRehman1 <148191554+AamirRehman1@users.noreply.github.com>
2023-11-28 10:55:12 -0800

    Update Phase2.py

--- Phase2.py
+++ Phase2.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -8,17 +8,11 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
-@app.route('/project/<project_name>')
-def project(project_name):
-    return render_template('project.html', project_name=project_name)
-
-@socketio.on('create_project')
-def create_project(data):
-    project_name = data['project_name']
-    emit('redirect', {'url': f'/project/{project_name}'})
+# Additional routes and socket events
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
+
 
 
 
