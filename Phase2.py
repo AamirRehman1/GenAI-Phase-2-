df4cf3b6c2fbc0c7c01aefa977d3242068e6f098
AamirRehman1 <148191554+AamirRehman1@users.noreply.github.com>
2023-11-28 12:08:15 -0800

    Update Phase2.py

--- Phase2.py
+++ Phase2.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '1224567'  # Remember to use a different key for production
+app.config['SECRET_KEY'] = '1224567'  # Use a different key for production
 
 socketio = SocketIO(app)
 
@@ -29,10 +30,5 @@ def test_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
-    socketio.run(app)
-
-
-
-
-
-
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
