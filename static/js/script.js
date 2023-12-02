fa0ff1eb895d9329904700f14f424e4b340b8ede
AamirRehman1 <148191554+AamirRehman1@users.noreply.github.com>
2023-12-02 14:34:47 -0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const projectNameForm = document.getElementById('project-name-form');
 
-    // Function to handle clicking on an editable task
     const editTask = (event) => {
         let task = event.target;
         let currentText = task.textContent;
@@ -15,22 +14,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
         inputField.onblur = () => {
             task.innerHTML = inputField.value;
-            // Add an event to save the new task value to your server here
         };
     };
 
-    // If we're on the index page with the form
+    const editTitle = (event) => {
+        let title = event.target;
+        let currentText = title.textContent;
+        let inputField = document.createElement('input');
+        inputField.type = 'text';
+        inputField.value = currentText;
+        inputField.className = 'title-input'; 
+        title.innerHTML = '';
+        title.appendChild(inputField);
+        inputField.focus();
+
+        inputField.onblur = () => {
+            title.innerHTML = inputField.value; 
+        };
+    };
+
     if (projectNameForm) {
         projectNameForm.onsubmit = (e) => {
             e.preventDefault();
             const projectName = document.getElementById('project-name').value;
-            // Redirect to the WBS diagram page with the project name
             window.location.href = `/wbs_diagram?project_name=${encodeURIComponent(projectName)}`;
         };
     } else {
-        // We're on the WBS page, so set up editable tasks
         document.querySelectorAll('.editable').forEach(task => {
             task.addEventListener('click', editTask);
+        });
+        document.querySelectorAll('.editable-title').forEach(title => { 
+            title.addEventListener('click', editTitle); 
         });
     }
 });
